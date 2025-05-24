822c9b0e78ab77100f46a2082cdbca2dc2cb902f
shiang <a300251992@gmail.com>
2025-05-24 17:24:10 +0800

    section2

--- all.js
+++ all.js
@@ -1,1 +1,21 @@
+const bannerSwiper = new Swiper('.banner-swiper', {
+  loop: true,
+  pagination: {
+    el: '.banner-swiper .swiper-pagination',
+    clickable: true
+  }
+})
 
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('.mySwiper', {
+    slidesPerView: 3, // 一次顯示三張
+    spaceBetween: 8,
+    loop: true, // 必開，才能從最後一張回到第一張
+    loopedSlides: 3, //loop 在少量圖片時也正常
+    slidesPerGroup: 1, // 每次移動一張
+    navigation: {
+      nextEl: '.thumb-button-next',
+      prevEl: '.thumb-button-prev'
+    }
+  })
+})
