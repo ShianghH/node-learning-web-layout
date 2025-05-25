6daca9cb0a3523f48a08ea8c0cf8d6b2a7af1be8
ShiangH <a300251992@gmail.com>
2025-05-26 01:33:20 +0800

    section3

--- all.js
+++ all.js
@@ -7,15 +7,17 @@ const bannerSwiper = new Swiper('.banner-swiper', {
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-  new Swiper('.mySwiper', {
-    slidesPerView: 3, // 一次顯示三張
-    spaceBetween: 8,
-    loop: true, // 必開，才能從最後一張回到第一張
-    loopedSlides: 3, //loop 在少量圖片時也正常
-    slidesPerGroup: 1, // 每次移動一張
-    navigation: {
-      nextEl: '.thumb-button-next',
-      prevEl: '.thumb-button-prev'
-    }
+  document.querySelectorAll('.mySwiper').forEach((swiperEl) => {
+    new Swiper(swiperEl, {
+      slidesPerView: 2.5,
+      spaceBetween: 8,
+      loop: true,
+      loopedSlides: 3,
+      slidesPerGroup: 1,
+      navigation: {
+        nextEl: swiperEl.parentElement.querySelector('.thumb-button-next'),
+        prevEl: swiperEl.parentElement.querySelector('.thumb-button-prev')
+      }
+    })
   })
 })
